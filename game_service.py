2d954b5199ccb04db4024cc3fe9108395567ca20
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2022-12-30 00:12:32 +0100

    TP3

--- game_service.py
+++ game_service.py
@@ -8,14 +8,15 @@ class GameService:
             game.add_player(Player(player_name, battle_field))
             return self.game_dao.create_game(game)
         def join_game(self, game_id: int, player_name: str) -> bool:
-            # à implementer
+            game=Game()
+            game.add_player(Player(player_name, game_id))
+            return self.game_dao.join_game(game)
         def get_game(self, game_id: int) -> Game:
-            # à implementer
-        def add_vessel(self, game_id: int, player_name: str, vessel_type: str, x: int, y: int, z: int) -> bool:
-            # à implementer
-        def shoot_at(self, game_id: int, shooter_name: str, vessel_id: int, x: int, y: int, z: int) -> bool:
-        # à implementer
-        def get_game_status(self, game_id: int, shooter_name: str) -> str:
-        # à implementer
+            game=Game(game_id)
+            return game_dao.get_game(game)
 
-        
+        def add_vessel(self, game_id: int, player_name: str, vessel_type: str, x: int, y: int, z: int) -> bool:
+            game = Game()
+            vessel = Vessel(vessel_type: str, x: int, y: int, z: int)
+            game.Player(player_name, game_id, vessel)
+            return game_dao.add_vessel(game)
